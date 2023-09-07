d5d5f484621aa81bab53e343f888f183f619b2a3
Diego <diegotimao1104@gmail.com>
2023-09-07 16:42:12 -0300

    refactor: Refatorando o código database.py

--- tech_news/database.py
+++ tech_news/database.py
@@ -1,8 +1,3 @@
-# Este é o arquivo de funções de acesso ao banco de dados. Basta importar e
-# chamar as funçoes
-# Atenção: este arquivo não deve ser alterado. Mudanças aqui não serão
-# refletidas no avaliador automático.
-
 from pymongo import MongoClient
 from decouple import config
 import copy
